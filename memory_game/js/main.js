94364a7679632e5e2ad8ed52173e0f2e1109611f
Adrian Thorburn <adrianthorburn@Adrians-MBP.gateway>
2019-03-09 17:00:07 +1100

    added JavaScript compatibility

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+// Java Starts here
+console.log("Up and running!") ;
+// Card Identification
+var cardOne = "queen" ;
+var cardTwo = "queen" ;
+var cardThree = "king" ;
+var cardFour = "king" ;
+
+// ConsoleLog
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);

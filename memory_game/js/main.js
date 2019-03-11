2d5729d09179d2a5fa344ef04328289912bd1535
Adrian Thorburn <adrianthorburn@Adrians-MBP.gateway>
2019-03-11 17:30:29 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,24 @@
 // Java Starts here
 console.log("Up and running!") ;
 // Card Identification
-var cardOne = "queen" ;
-var cardTwo = "queen" ;
-var cardThree = "king" ;
-var cardFour = "king" ;
+//var cardOne = "queen" ;
+//var cardTwo = "queen" ;
+//var cardThree = "king" ;
+//var cardFour = "king" ;
+var cards = [ "queen" , "queen" , "king" , "king" ] ;
+var cardsInPlay = [] ;
+var cardOne = cards[0];
+cardsInPlay.push('cardOne') ;
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo') ;
+//Write an if statement that checks to see if the length of the cardsInPlay array is 2. 
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+	alert("You found a match!");
+} else {
+	alert("sorry try again :(");
+}
+
 
 // ConsoleLog
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("user flipped" + " " + cardOne) ;
+console.log("user flipped" + " " + cardTwo) ;
